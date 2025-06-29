871dcac136796b25d6168e40295cf6dff8232b9a
Nazrul <nihalahmed@gmail.com>
2025-06-29 13:19:46 +0530

    Initial file

---
+++ bankacc.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route for the root URL "/"
+@app.route('/')
+def hello():
+    return 'Hello Python!'
+
+# Run the app when the script is executed directly
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
